7ebe3cf04dc9a7c16d0a946ac41fcb9afea8a34f
Arun <maskedman9817@gmail.com>
2024-04-20 15:39:13 +0500

    fixed staticfiles issue

--- core/settings/base.py
+++ core/settings/base.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(ROOT_BASE_DIR, 'static'))
+STATICFILES_DIRS = (os.path.join(ROOT_BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(ROOT_BASE_DIR,'staticfiles')
 
 STATICFILES_FINDERS = (
